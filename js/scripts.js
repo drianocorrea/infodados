36979877201c93b401199648434b6686dd2c7e43
iamjoao <joaorrp46@gmail.com>
2020-03-26 03:34:51 -0300

    Alteração no script.js para receber os valores html

--- js/scripts.js
+++ js/scripts.js
@@ -24,8 +24,7 @@
 
         //função chamada quando o usario clica no botão
         function cadUser(){
-            alert("jfkaskfa")
-            id = Math.floor(Math.random() * 10000000)
+            id = Math.floor(Math.random() * 10000000) //pegando um numero aleatório entre 0 e 10000000 *não recomendado mas por enquanto vai assim msm*
             nome = document.getElementById("nome").value;
             data = document.getElementById("data").value;
             email = document.getElementById("email").value;
@@ -36,6 +35,7 @@
             //if para controlar a quantidade de contas criadas
             if(c1 == c+1){
                 user[c] = new usuario(id, nome, data, email, telefone, login, senha); //armazendo os dados no vetor
+                console.log(user[c]); //console.log para mostrar que está funcionando
                 c = c+1;
                 c1 = c1+1;
             }
